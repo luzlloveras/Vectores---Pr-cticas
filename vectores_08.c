8fbe1235c69369022865e2df5d35ce835c20dd89
luzlloveras <luzlloveras7@gmail.com>
2020-11-27 16:26:26 -0300

    Update vectores_08.c

--- vectores_08.c
+++ vectores_08.c
@@ -27,9 +27,7 @@ void CARGAR (int vector[], int numero)
 {
     int i;
     for (i = 0; i < numero; i++)
-    {
         vector[i] = rand() % 100;
-    }
 }
 
 void MIRAR (int vector[], int numero)
@@ -37,14 +35,10 @@ void MIRAR (int vector[], int numero)
     int i;
     printf("Impresion de los datos\n");
     for (i = 0; i < numero; i++)
-    {
         printf("%4d", i);
-    }
     printf("\n");
     for (i = 0; i < numero; i++)
-    {
         printf("%4d", vector[i]);
-    }
     printf("\n");
 }  
 
@@ -53,11 +47,7 @@ int MINIMO (int vector[], int numero)
     int i, valor_minimo = vector[0];
 
     for (i = 1; i < numero; i++)
-    {
         if (vector[i] < valor_minimo)
-        {
             valor_minimo = vector[i];
-        }
-    }
     return valor_minimo;
 }  
