d7f8a5d8ee7d0c6e305bb2b1ac2fbdef2922a46a
luzlloveras <luzlloveras7@gmail.com>
2020-11-27 16:22:26 -0300

    Update vectores_01.c

--- vectores_01.c
+++ vectores_01.c
@@ -22,26 +22,18 @@ int main()
 
     //  Imprime los valores de cada posicion del vector 
     for (i = 0; i < numero ; i++)
-    {
         printf("%4d", vector[i]);
-    }
 
     // Calcula promedio
     for (i = 0; i < numero; i++)
-    {
         suma += vector[i];
-    }
     promedio = (float)suma / numero;
     printf("\n El promedio es: %.2f \n", promedio);
 
     // Calcula cantidad de valores que superan el promedio
     for (i = 0; i < numero; i++)
-    {
         if (vector[i] > promedio)
-        {
             contador++;
-        }
-    }
     printf("Hay %d que superan el promedio de %.2f \n", contador, promedio);
 
     return 0;
