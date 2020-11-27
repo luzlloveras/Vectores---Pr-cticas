88bd92ccbc7bafd7b99b69f3cb724b0b2b599373
luzlloveras <luzlloveras7@gmail.com>
2020-11-27 16:28:07 -0300

    Update vectores_10.c

--- vectores_10.c
+++ vectores_10.c
@@ -27,9 +27,7 @@ void CARGAR (int vec[], int n)
 {
     int i;
     for (i = 0; i < n; i++)
-    {
         vector[i] = rand() % 100;
-    }
 }
 
 // Se imprimen los valores de cada posicion del vector
@@ -37,14 +35,10 @@ void MIRAR (int vec[], int n)
 {
     int i;
     for (i = 0; i < n; i++)
-    {
         printf("%6d", i);
-    }
     printf("\n");
     for (i = 0; i < n; i++)
-    {
         printf("%6d", vec[i]);
-    }
     printf("\n");
 }
 
@@ -53,12 +47,8 @@ void INTERCAMBIO (int vec[], int n)
 {
     int i, aux, posicion_minimo = 0;
     for (i = 1; i < n; i++)
-    {
         if (vec[i] < vec[posicion_minimo])
-        {
             posicion_minimo = i;
-        }
-    }
     aux = vec[0];
     vec[0] = vec[posicion_minimo];
     vec[posicion_minimo] = aux;
